8af499e5361785bffba9bd8aff5a346354b06340
Lego <LaClarence@users.noreply.github.com>
2019-01-28 21:23:52 +0100

    Code review on cancel. remove hard coded values

--- index.js
+++ index.js
@@ -21,6 +21,110 @@ slots = [
   "1730"
 ];
 
+const generateDefaultSlots = slots => {
+  let defaultSlots = {};
+  slots.forEach(s => {
+    defaultSlots[s] = { isAvailable: true };
+  });
+  return defaultSlots;
+};
+
+const booking = [];
+
+const sendError = (res, info, statusCode) => {
+  const errMessage = `Bad request: ${info}`;
+  return res.status(statusCode).json({
+    error: {
+      message: errMessage
+    }
+  });
+};
+
+const checkAvailabilities = dateReq => {
+  let found = booking.find(elt => elt.date === dateReq);
+  if (found === undefined) {
+    found = { date: dateReq, slots: generateDefaultSlots(slots) };
+    booking.push(found);
+  }
+  return found;
+};
+
+const isDateValid = date => {
+  return datefns.isPast(date) === false && datefns.isSunday(date) === false;
+};
+
+const notValidDateresponse = res => {
+  return sendError(res, "Provided date is in the past or a sunday!", 400);
+};
+
+app.get("/visites", (req, res) => {
+  let date = req.query.date;
+  console.log(`GET request on /visites on date ${date}...`);
+  if (isDateValid(date) === false) {
+    return notValidDateresponse(res);
+  }
+  res.json(checkAvailabilities(date));
+});
+
+// Book a slot
+app.post("/visites", (req, res) => {
+  console.log("POST request to book a slotReceived availabilities request");
+  const on = req.body.date;
+  if (isDateValid(on) === false) {
+    return notValidDateResponse(res);
+  }
+  const atSlot = req.body.slot;
+  if (slots.includes(atSlot) === false) {
+    return sendError(res, "Provided slot is not valid!", 400);
+  }
+  const who = req.body.name;
+  if (!who) {
+    return sendError(res, "Provided name is not valid!", 400);
+  }
+  let onDate = checkAvailabilities(on);
+  if (onDate.slots[atSlot].isAvailable === false) {
+    return sendError(res, "Slot already booked!", 400);
+  }
+  var milliseconds = new Date().getTime();
+  onDate.slots[atSlot] = { isAvailable: false, name: who, key: milliseconds };
+  res.json({ message: "Successfuly booked", key: milliseconds });
+});
+
+app.get("/visites/cancel", (req, res) => {
+  console.log("GET cancel request on visites...");
+  let key = Number(req.query.key);
+  console.log(`Cancel slot with key is ${key}`);
+  for (let i = 0; i < booking.length; i++) {
+    for (let [slot, avail] of Object.entries(booking[i].slots)) {
+      if (avail.key === key) {
+        let info = `The booking done on the ${booking[i].date} at ${slot} by ${
+          avail.name
+        } is now cancelled.`;
+        booking[i].slots[slot] = { isAvailable: true };
+        console.log(info);
+        return res.json({
+          message: info
+        });
+      }
+    }
+  }
+  res.json({
+    message: "Key not found, the slot is not cancelled!"
+  });
+});
+
+app.all("*", function(req, res) {
+  sendError(res, "Page not found!", 404);
+});
+
+app.listen(3000, () => {
+  console.log(
+    ">-(*)-(*)-(*)-(*)- Exo Doctoliv server started... -(*)-(*)-(*)-(*)-<"
+  );
+});
+
+/*
+
 const defaultSlots = {
   "1000": { isAvailable: true },
   "1030": { isAvailable: true },
@@ -36,7 +140,8 @@ const defaultSlots = {
   "1730": { isAvailable: true }
 };
 
-const booking = [
+
+
   {
     date: "2019-01-31",
     slots: {
@@ -71,66 +176,27 @@ const booking = [
       "1730": { isAvailable: true }
     }
   }
-];
 
-const sendError = (res, info, statusCode) => {
-  const errMessage = `Bad request: ${info}`;
-  return res.status(statusCode).json({
-    error: {
-      message: errMessage
+app.get("/visites/cancel", (req, res) => {
+  console.log("GET cancel request on visites...");
+  let key = Number(req.query.key);
+  console.log(`Cancel slot with key is ${key}`);
+  for (let i = 0; i < booking.length; i++) {
+    let slotKeys = Object.keys(booking[i].slots);
+    for (let j = 0; j < slotKeys.length; j++) {
+      let slotKey = booking[i].slots[slotKeys[j]].key;
+      if (slotKey && slotKey === key) {
+        console.log("Slot found: ", booking[i].slots[slotKeys[j]]);
+        booking[i].slots[slotKeys[j]] = { isAvailable: true };
+        return res.json({
+          message: "Visit is cancelled."
+        });
+      }
     }
+  }
+  res.json({
+    message: "Key not found, the slot is not cancelled!"
   });
-};
-
-const checkAvailabilities = dateReq => {
-  let found = booking.find(elt => elt.date === dateReq);
-  if (found === undefined) {
-    found = { date: dateReq, slots: defaultSlots };
-    booking.push(found);
-  }
-  return found;
-};
-
-app.get("/visites", (req, res) => {
-  console.log("GET request on visites...");
-  let date = req.query.date;
-  if (datefns.isPast(date) === true) {
-    return sendError(res, "Provided date is in the past!", 400);
-  }
-
-  res.json(checkAvailabilities(date));
 });
 
-// Book a slot
-app.post("/visites", (req, res) => {
-  console.log("POST request to book a slotReceived availabilities request");
-  const on = req.body.date;
-  if (datefns.isPast(on) === true) {
-    return sendError(res, "Provided date is in the past!", 400);
-  }
-  const atSlot = req.body.slot;
-  if (slots.includes(atSlot) === false) {
-    return sendError(res, "Provided slot is not valid!", 400);
-  }
-  const who = req.body.name;
-  if (!who) {
-    return sendError(res, "Provided name is not valid!", 400);
-  }
-  let onDate = checkAvailabilities(on);
-  if (onDate.slots[atSlot].isAvailable === false) {
-    return sendError(res, "Slot already booked!", 400);
-  }
-  onDate.slots[atSlot] = { isAvailable: false, name: who };
-  res.json({ message: "Successfuly booked" });
-});
-
-// Gérer les pages introuvables
-app.all("*", function(req, res) {
-  sendError(res, "Page not found!", 404);
-});
-
-app.listen(3000, () => {
-  console.log("**********************************");
-  console.log("* Exo Doctoliv server started... *");
-  console.log("**********************************");
-});
+*/
